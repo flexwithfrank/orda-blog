7fa51292cfbff75fafdd134a72997dcd7a53fb58
Rodolpho Bravo <bravorodolpho@gmail.com>
2021-11-23 17:29:02 -0400

    Add Text Based Page

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -45,3 +45,45 @@ export function getAllPosts(fields: string[] = []) {
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
 }
+
+const pagesDirectory = join(process.cwd(), "TextBasedPages")
+
+export function getPageSlugs() {
+  return fs.readdirSync(pagesDirectory)
+}
+
+export function getPageBySlug(slug: string, fields: string[] = []) {
+  const realSlug = slug.replace(/\.md$/, "")
+  const fullPath = join(pagesDirectory, `${realSlug}.md`)
+  const fileContents = fs.readFileSync(fullPath, "utf8")
+  const { data, content } = matter(fileContents)
+
+  type Items = {
+    [key: string]: string
+  }
+
+  const items: Items = {}
+
+  // Ensure only the minimal needed data is exposed
+  fields.forEach((field) => {
+    if (field === "slug") {
+      items[field] = realSlug
+    }
+    if (field === "content") {
+      items[field] = content
+    }
+
+    if (typeof data[field] !== "undefined") {
+      items[field] = data[field]
+    }
+  })
+
+  return items
+}
+
+export function getAllPages(fields: string[] = []) {
+  const slugs = getPageSlugs()
+  const pages = slugs.map((slug) => getPageBySlug(slug, fields))
+
+  return pages
+}
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,62 @@
+import { getAllPages, getPageBySlug } from "lib/api"
+import markdownToHtml from "lib/markdownToHtml"
+import { Footer } from "components/Footer"
+import { Header } from "components/Header"
+
+export default function TextBasedPage({ page }) {
+  return (
+    <>
+      <Header />
+      <div className="w-11/12 mx-auto flex flex-col justify-center items-center my-16">
+        <article className="prose lg:prose-xl">
+          <h1>{page.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: page.content }} />
+        </article>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const page = getPageBySlug(params.slug, [
+    "title",
+    "date",
+    "slug",
+    "author",
+    "content",
+    "ogImage",
+    "coverImage",
+  ])
+  const content = await markdownToHtml(page.content || "")
+
+  return {
+    props: {
+      page: {
+        ...page,
+        content,
+      },
+    },
+    revalidate: 10,
+  }
+}
+
+export async function getStaticPaths() {
+  const pages = getAllPages(["slug"])
+  return {
+    paths: pages.map((page) => {
+      return {
+        params: {
+          slug: page.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}

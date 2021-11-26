1ce1a4c0ba5a39dc76a881f5549b80077c8dca3b
Rodolpho Bravo <bravorodolpho@gmail.com>
2021-11-26 08:35:30 -0400

    Adjust style of ReadyToLaunch component

--- src/components/ReadyToLaunch/index.tsx
+++ src/components/ReadyToLaunch/index.tsx
@@ -19,10 +19,12 @@ export function ReadyToLaunch({
   return (
     <div className="relative z-0 w-full my-24">
       <div className="relative bg-darker-blue sm:max-w-7xl h-full sm:h-48 w-11/12 mx-auto px-4 py-6 sm:px-6 rounded-3xl lg:py-16 lg:px-8">
-        <div className="relative w-11/12 flex flex-col sm:flex-row justify-between h-full items-center mx-auto z-20">
+        <div className="relative w-11/12 px-0 md:px-4 flex flex-col sm:flex-row justify-between h-full items-center mx-auto z-20">
           <div>
             <div className="text-5xl font-extrabold text-white">{ctaText}</div>
-            {subDescription && <p className="text-white">{subDescription}</p>}
+            {subDescription && (
+              <p className="text-white mt-2">{subDescription}</p>
+            )}
           </div>
           <div className="flex flex-col items-center mt-8 sm:mt-0 sm:ml-auto">
             <SignUpButton bgColor="white" textColor="black" />
@@ -68,7 +70,7 @@ export function ReadyToLaunch({
         {option2 ? null : (
           <svg
             viewBox="0 0 100 100"
-            className="absolute top-8 left-8 h-20 w-20 fill-current text-light-blue z-0"
+            className="absolute top-6 md:top-8 left-10 md:left-14 h-20 w-20 fill-current text-light-blue z-0"
             xmlns="http://www.w3.org/2000/svg"
           >
             <circle cx="50" cy="50" r="50" />
